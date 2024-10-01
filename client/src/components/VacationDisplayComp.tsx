5e5a6eedb5657764837c032dc76054974b9eb054
Kalifer783 <kik8bgork@gmail.com>
2024-10-01 14:50:41 +0000

    Began adding in a vacation page.

---
+++ client/src/components/VacationDisplayComp.tsx
@@ -0,0 +1,9 @@
+import {Vacation } from './VacationComp';
+
+type VacationDisplayCompProp {
+    vacations: Array<Vacation>;
+}
+
+const VacationDisplayComp = ({vacations} : VacationDisplayCompProp) => {
+
+}
